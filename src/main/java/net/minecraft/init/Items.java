c98eb5a4702f1e48bec0452481413f60211e355b
BriceFab <bricefab123@gmail.com>
2020-04-13 04:39:36 +0200

    sceptre creeper spawn

--- src/main/java/net/minecraft/init/Items.java
+++ src/main/java/net/minecraft/init/Items.java
@@ -1,20 +1,10 @@
 package net.minecraft.init;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemArmorStand;
-import net.minecraft.item.ItemBow;
-import net.minecraft.item.ItemEmptyMap;
-import net.minecraft.item.ItemEnchantedBook;
-import net.minecraft.item.ItemFishingRod;
-import net.minecraft.item.ItemMap;
-import net.minecraft.item.ItemPotion;
-import net.minecraft.item.ItemShears;
+import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.scandicraft.items.ItemSpawnScepter;
 
-public class Items
-{
+public class Items {
     public static final Item iron_shovel;
     public static final Item iron_pickaxe;
     public static final Item iron_axe;
@@ -241,25 +231,20 @@ public class Items
     public static final Item bloody_sword;
     public static final ItemSpawnScepter scepter_creeper;
 
-    private static Item getRegisteredItem(String name)
-    {
-        return (Item)Item.itemRegistry.getObject(new ResourceLocation(name));
+    private static Item getRegisteredItem(String name) {
+        return (Item) Item.itemRegistry.getObject(new ResourceLocation(name));
     }
 
-    static
-    {
-        if (!Bootstrap.isRegistered())
-        {
+    static {
+        if (!Bootstrap.isRegistered()) {
             throw new RuntimeException("Accessed Items before Bootstrap!");
-        }
-        else
-        {
+        } else {
             iron_shovel = getRegisteredItem("iron_shovel");
             iron_pickaxe = getRegisteredItem("iron_pickaxe");
             iron_axe = getRegisteredItem("iron_axe");
             flint_and_steel = getRegisteredItem("flint_and_steel");
             apple = getRegisteredItem("apple");
-            bow = (ItemBow)getRegisteredItem("bow");
+            bow = (ItemBow) getRegisteredItem("bow");
             arrow = getRegisteredItem("arrow");
             coal = getRegisteredItem("coal");
             diamond = getRegisteredItem("diamond");
@@ -296,26 +281,26 @@ public class Items
             wheat_seeds = getRegisteredItem("wheat_seeds");
             wheat = getRegisteredItem("wheat");
             bread = getRegisteredItem("bread");
-            leather_helmet = (ItemArmor)getRegisteredItem("leather_helmet");
-            leather_chestplate = (ItemArmor)getRegisteredItem("leather_chestplate");
-            leather_leggings = (ItemArmor)getRegisteredItem("leather_leggings");
-            leather_boots = (ItemArmor)getRegisteredItem("leather_boots");
-            chainmail_helmet = (ItemArmor)getRegisteredItem("chainmail_helmet");
-            chainmail_chestplate = (ItemArmor)getRegisteredItem("chainmail_chestplate");
-            chainmail_leggings = (ItemArmor)getRegisteredItem("chainmail_leggings");
-            chainmail_boots = (ItemArmor)getRegisteredItem("chainmail_boots");
-            iron_helmet = (ItemArmor)getRegisteredItem("iron_helmet");
-            iron_chestplate = (ItemArmor)getRegisteredItem("iron_chestplate");
-            iron_leggings = (ItemArmor)getRegisteredItem("iron_leggings");
-            iron_boots = (ItemArmor)getRegisteredItem("iron_boots");
-            diamond_helmet = (ItemArmor)getRegisteredItem("diamond_helmet");
-            diamond_chestplate = (ItemArmor)getRegisteredItem("diamond_chestplate");
-            diamond_leggings = (ItemArmor)getRegisteredItem("diamond_leggings");
-            diamond_boots = (ItemArmor)getRegisteredItem("diamond_boots");
-            golden_helmet = (ItemArmor)getRegisteredItem("golden_helmet");
-            golden_chestplate = (ItemArmor)getRegisteredItem("golden_chestplate");
-            golden_leggings = (ItemArmor)getRegisteredItem("golden_leggings");
-            golden_boots = (ItemArmor)getRegisteredItem("golden_boots");
+            leather_helmet = (ItemArmor) getRegisteredItem("leather_helmet");
+            leather_chestplate = (ItemArmor) getRegisteredItem("leather_chestplate");
+            leather_leggings = (ItemArmor) getRegisteredItem("leather_leggings");
+            leather_boots = (ItemArmor) getRegisteredItem("leather_boots");
+            chainmail_helmet = (ItemArmor) getRegisteredItem("chainmail_helmet");
+            chainmail_chestplate = (ItemArmor) getRegisteredItem("chainmail_chestplate");
+            chainmail_leggings = (ItemArmor) getRegisteredItem("chainmail_leggings");
+            chainmail_boots = (ItemArmor) getRegisteredItem("chainmail_boots");
+            iron_helmet = (ItemArmor) getRegisteredItem("iron_helmet");
+            iron_chestplate = (ItemArmor) getRegisteredItem("iron_chestplate");
+            iron_leggings = (ItemArmor) getRegisteredItem("iron_leggings");
+            iron_boots = (ItemArmor) getRegisteredItem("iron_boots");
+            diamond_helmet = (ItemArmor) getRegisteredItem("diamond_helmet");
+            diamond_chestplate = (ItemArmor) getRegisteredItem("diamond_chestplate");
+            diamond_leggings = (ItemArmor) getRegisteredItem("diamond_leggings");
+            diamond_boots = (ItemArmor) getRegisteredItem("diamond_boots");
+            golden_helmet = (ItemArmor) getRegisteredItem("golden_helmet");
+            golden_chestplate = (ItemArmor) getRegisteredItem("golden_chestplate");
+            golden_leggings = (ItemArmor) getRegisteredItem("golden_leggings");
+            golden_boots = (ItemArmor) getRegisteredItem("golden_boots");
             flint = getRegisteredItem("flint");
             porkchop = getRegisteredItem("porkchop");
             cooked_porkchop = getRegisteredItem("cooked_porkchop");
@@ -349,7 +334,7 @@ public class Items
             furnace_minecart = getRegisteredItem("furnace_minecart");
             egg = getRegisteredItem("egg");
             compass = getRegisteredItem("compass");
-            fishing_rod = (ItemFishingRod)getRegisteredItem("fishing_rod");
+            fishing_rod = (ItemFishingRod) getRegisteredItem("fishing_rod");
             clock = getRegisteredItem("clock");
             glowstone_dust = getRegisteredItem("glowstone_dust");
             fish = getRegisteredItem("fish");
@@ -361,8 +346,8 @@ public class Items
             bed = getRegisteredItem("bed");
             repeater = getRegisteredItem("repeater");
             cookie = getRegisteredItem("cookie");
-            filled_map = (ItemMap)getRegisteredItem("filled_map");
-            shears = (ItemShears)getRegisteredItem("shears");
+            filled_map = (ItemMap) getRegisteredItem("filled_map");
+            shears = (ItemShears) getRegisteredItem("shears");
             melon = getRegisteredItem("melon");
             pumpkin_seeds = getRegisteredItem("pumpkin_seeds");
             melon_seeds = getRegisteredItem("melon_seeds");
@@ -383,7 +368,7 @@ public class Items
             ghast_tear = getRegisteredItem("ghast_tear");
             gold_nugget = getRegisteredItem("gold_nugget");
             nether_wart = getRegisteredItem("nether_wart");
-            potionitem = (ItemPotion)getRegisteredItem("potion");
+            potionitem = (ItemPotion) getRegisteredItem("potion");
             glass_bottle = getRegisteredItem("glass_bottle");
             spider_eye = getRegisteredItem("spider_eye");
             fermented_spider_eye = getRegisteredItem("fermented_spider_eye");
@@ -405,7 +390,7 @@ public class Items
             potato = getRegisteredItem("potato");
             baked_potato = getRegisteredItem("baked_potato");
             poisonous_potato = getRegisteredItem("poisonous_potato");
-            map = (ItemEmptyMap)getRegisteredItem("map");
+            map = (ItemEmptyMap) getRegisteredItem("map");
             golden_carrot = getRegisteredItem("golden_carrot");
             skull = getRegisteredItem("skull");
             carrot_on_a_stick = getRegisteredItem("carrot_on_a_stick");
@@ -413,13 +398,13 @@ public class Items
             pumpkin_pie = getRegisteredItem("pumpkin_pie");
             fireworks = getRegisteredItem("fireworks");
             firework_charge = getRegisteredItem("firework_charge");
-            enchanted_book = (ItemEnchantedBook)getRegisteredItem("enchanted_book");
+            enchanted_book = (ItemEnchantedBook) getRegisteredItem("enchanted_book");
             comparator = getRegisteredItem("comparator");
             netherbrick = getRegisteredItem("netherbrick");
             quartz = getRegisteredItem("quartz");
             tnt_minecart = getRegisteredItem("tnt_minecart");
             hopper_minecart = getRegisteredItem("hopper_minecart");
-            armor_stand = (ItemArmorStand)getRegisteredItem("armor_stand");
+            armor_stand = (ItemArmorStand) getRegisteredItem("armor_stand");
             iron_horse_armor = getRegisteredItem("iron_horse_armor");
             golden_horse_armor = getRegisteredItem("golden_horse_armor");
             diamond_horse_armor = getRegisteredItem("diamond_horse_armor");
@@ -462,24 +447,24 @@ public class Items
             lazurite_pickaxe = getRegisteredItem("lazurite_pickaxe");
             lazurite_axe = getRegisteredItem("lazurite_axe");
             lazurite_hoe = getRegisteredItem("lazurite_hoe");
-            scandium_helmet = (ItemArmor)getRegisteredItem("scandium_helmet");
-            scandium_chestplate = (ItemArmor)getRegisteredItem("scandium_chestplate");
-            scandium_leggings = (ItemArmor)getRegisteredItem("scandium_leggings");
-            scandium_boots = (ItemArmor)getRegisteredItem("scandium_boots");
-            pyrite_helmet = (ItemArmor)getRegisteredItem("pyrite_helmet");
-            pyrite_chestplate = (ItemArmor)getRegisteredItem("pyrite_chestplate");
-            pyrite_leggings = (ItemArmor)getRegisteredItem("pyrite_leggings");
-            pyrite_boots = (ItemArmor)getRegisteredItem("pyrite_boots");
-            lazurite_helmet = (ItemArmor)getRegisteredItem("lazurite_helmet");
-            lazurite_chestplate = (ItemArmor)getRegisteredItem("lazurite_chestplate");
-            lazurite_leggings = (ItemArmor)getRegisteredItem("lazurite_leggings");
-            lazurite_boots = (ItemArmor)getRegisteredItem("lazurite_boots");
-            bloody_helmet = (ItemArmor)getRegisteredItem("bloody_helmet");
-            bloody_chestplate = (ItemArmor)getRegisteredItem("bloody_chestplate");
-            bloody_leggings = (ItemArmor)getRegisteredItem("bloody_leggings");
-            bloody_boots = (ItemArmor)getRegisteredItem("bloody_boots");
+            scandium_helmet = (ItemArmor) getRegisteredItem("scandium_helmet");
+            scandium_chestplate = (ItemArmor) getRegisteredItem("scandium_chestplate");
+            scandium_leggings = (ItemArmor) getRegisteredItem("scandium_leggings");
+            scandium_boots = (ItemArmor) getRegisteredItem("scandium_boots");
+            pyrite_helmet = (ItemArmor) getRegisteredItem("pyrite_helmet");
+            pyrite_chestplate = (ItemArmor) getRegisteredItem("pyrite_chestplate");
+            pyrite_leggings = (ItemArmor) getRegisteredItem("pyrite_leggings");
+            pyrite_boots = (ItemArmor) getRegisteredItem("pyrite_boots");
+            lazurite_helmet = (ItemArmor) getRegisteredItem("lazurite_helmet");
+            lazurite_chestplate = (ItemArmor) getRegisteredItem("lazurite_chestplate");
+            lazurite_leggings = (ItemArmor) getRegisteredItem("lazurite_leggings");
+            lazurite_boots = (ItemArmor) getRegisteredItem("lazurite_boots");
+            bloody_helmet = (ItemArmor) getRegisteredItem("bloody_helmet");
+            bloody_chestplate = (ItemArmor) getRegisteredItem("bloody_chestplate");
+            bloody_leggings = (ItemArmor) getRegisteredItem("bloody_leggings");
+            bloody_boots = (ItemArmor) getRegisteredItem("bloody_boots");
             bloody_sword = getRegisteredItem("bloody_sword");
-            scepter_creeper =(ItemSpawnScepter) getRegisteredItem("scepter_sword");
+            scepter_creeper = (ItemSpawnScepter) getRegisteredItem("scepter_creeper");
         }
     }
 }
--- src/main/java/net/scandicraft/items/ItemSpawnScepter.java
+++ src/main/java/net/scandicraft/items/ItemSpawnScepter.java
@@ -1,104 +1,67 @@
 package net.scandicraft.items;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.*;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MathHelper;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ItemSpawnScepter extends Item {
-    private float attackDamage;
-    private static final Logger logger = LogManager.getLogger();
+    private final int CREEPER_METADATA = 50;
 
-    public ItemSpawnScepter(int maxUse) {
-
-        this.maxStackSize = 1;
-        this.setMaxDamage(maxUse);
+    public ItemSpawnScepter(int max_uses) {
         this.setCreativeTab(CreativeTabs.tabCombat);
-
+        this.setMaxDamage(max_uses);
+        this.setMaxStackSize(1);
     }
 
     /**
-     * Returns the amount of damage this item will deal. One heart of damage is equal to 2 damage points.
+     * Called when a Block is right-clicked with this Item
      */
+    public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if (worldIn.isRemote) {
+            return true;
+        } else if (!playerIn.canPlayerEdit(pos.offset(side), side, stack)) {
+            return false;
+        } else {
+            IBlockState iblockstate = worldIn.getBlockState(pos);
 
+            pos = pos.offset(side);
+            double d0 = 0.0D;
 
-    /**
-     * Current implementations of this method in child classes do not use the entry argument beside ev. They just raise
-     * the damage on the stack.
-     */
-    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-        stack.damageItem(1, attacker);
-        return true;
-    }
+            if (side == EnumFacing.UP && iblockstate instanceof BlockFence) {
+                d0 = 0.5D;
+            }
 
-    /**
-     * Called when a Block is destroyed using this Item. Return true to trigger the "Use Item" statistic.
-     */
-    public boolean onBlockDestroyed(ItemStack stack, World worldIn, Block blockIn, BlockPos pos, EntityLivingBase playerIn) {
-        if ((double) blockIn.getBlockHardness(worldIn, pos) != 0.0D) {
-            stack.damageItem(2, playerIn);
+            Entity entity = spawnCreature(worldIn, CREEPER_METADATA, (double) pos.getX() + 0.5D, (double) pos.getY() + d0, (double) pos.getZ() + 0.5D);
+
+            if (entity != null) {
+                if (entity instanceof EntityLivingBase && stack.hasDisplayName()) {
+                    entity.setCustomNameTag(stack.getDisplayName());
+                }
+            }
+
+            return true;
         }
-
-        return true;
     }
 
     /**
-     * Returns True is the item is renderer in full 3D when hold.
+     * Spawns the creature specified by the egg's type in the location specified by the last three parameters.
+     * Parameters: world, entityID, x, y, z.
      */
-    public boolean isFull3D() {
-        return true;
-    }
-
-    /**
-     * returns the action that specifies what animation to play when the items is being used
-     */
-    public EnumAction getItemUseAction(ItemStack stack) {
-
-        return EnumAction.BLOCK;
-    }
-
-    /**
-     * How long it takes to use or consume an item
-     */
-    public int getMaxItemUseDuration(ItemStack stack) {
-        return 72000;
-    }
-
-    /**
-     * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
-     */
-    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
-    {
-        logger.info("CLICK");
-        MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(worldIn, playerIn, true);
-        if (movingobjectposition == null)
-        {
-            return itemStackIn;
-        }
-        BlockPos blockpos = movingobjectposition.getBlockPos();
-        Entity entity = spawnCreature(worldIn, itemStackIn.getMetadata(), (double)blockpos.getX() + 0.5D, (double)blockpos.getY() + 0.5D, (double)blockpos.getZ() + 0.5D);
-
-       return  itemStackIn;
-    }
-    public static Entity spawnCreature(World worldIn, int entityID, double x, double y, double z)
-    {
-
-
-            Entity entity = null;
+    public static Entity spawnCreature(World worldIn, int entityID, double x, double y, double z) {
+        Entity entity = null;
 
         for (int i = 0; i < 1; ++i) {
-            entity = EntityList.createEntityByID(50, worldIn);
+            entity = EntityList.createEntityByID(entityID, worldIn);
 
-            if (entity instanceof EntityLivingBase) {
+            if (entity != null && entity instanceof EntityLivingBase) {
                 EntityLiving entityliving = (EntityLiving) entity;
                 entity.setLocationAndAngles(x, y, z, MathHelper.wrapAngleTo180_float(worldIn.rand.nextFloat() * 360.0F), 0.0F);
                 entityliving.rotationYawHead = entityliving.rotationYaw;
@@ -106,13 +69,9 @@ public class ItemSpawnScepter extends Item {
                 entityliving.onInitialSpawn(worldIn.getDifficultyForLocation(new BlockPos(entityliving)), (IEntityLivingData) null);
                 worldIn.spawnEntityInWorld(entity);
                 entityliving.playLivingSound();
-                logger.info("SPAWN : " + entity);
             }
-
-
         }
-            return  entity;
+
+        return entity;
     }
-
-
 }
